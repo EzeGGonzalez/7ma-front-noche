b432ef51db07b3637188a8bd85faca2d3bd4501b
Ezequiel Gerónimo Gonzalez <ezequiel@adaitw.com.ar>
2019-11-09 09:44:15 -0300

    ejercicio practica dom

---
+++ ejercicios/shoppingCart/index.js
@@ -0,0 +1,43 @@
+var products = [{
+    id: 1,
+    name: "Dolor est.",
+    description: "Amet stet sanctus amet eos eirmod amet. Et amet ipsum.",
+    imageUrl: "https://picsum.photos/200/300?random=1",
+    price: 799
+  },
+  {
+    id: 2,
+    name: "Sit.",
+    description: "Ipsum ipsum no et ut ipsum sed diam tempor. Duo.",
+    imageUrl: "https://picsum.photos/200/300?random=2",
+    price: 349,
+  },
+  {
+    id: 3,
+    name: "Lorem invidunt.",
+    description: "Kasd lorem rebum nonumy tempor sanctus invidunt. Amet et tempor.",
+    imageUrl: "https://picsum.photos/200/300?random=3",
+    price: 1499
+  },
+  {
+    id: 4,
+    name: "Erat est clita.",
+    description: "Invidunt sit eirmod at kasd et. Dolore lorem sadipscing voluptua.",
+    imageUrl: "https://picsum.photos/200/300?random=4",
+    price: 999
+  },
+  {
+    id: 5,
+    name: "Sed.",
+    description: "Dolor takimata diam sed vero lorem sit no et lorem.",
+    imageUrl: "https://picsum.photos/200/300?random=5",
+    price: 599
+  },
+  {
+    id: 6,
+    name: "Sanctus dolores.",
+    description: "Sed clita labore invidunt sanctus duo aliquyam elitr sit dolores,.",
+    imageUrl: "https://picsum.photos/200/300?random=6",
+    price: 499
+  }
+]
