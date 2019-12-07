80cc7390c65048fc835cd81f900fa7d404c5da80
Ezequiel Gerónimo Gonzalez <ezequiel@adaitw.com.ar>
2019-12-07 10:57:12 -0300

    ejercicio de APIs

---
+++ promises/05-ajax-listUsers/index.js
@@ -0,0 +1,21 @@
+const cargarDatos = () => {
+  axios
+    .get('https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole')
+    .then((response) => {
+      const users = response.data;
+
+      users.forEach(u => {
+        const userHTML = `
+          <tr>
+            <td>${u.first}</td>
+            <td>${u.last}</td>
+            <td>${u.email}</td>
+          </tr>
+        `;
+
+        document.querySelector('#users-tbody').innerHTML += userHTML;
+      });
+    })
+}
+
+document.getElementById('btn-cargar').onclick = cargarDatos;
